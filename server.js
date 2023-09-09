83a72f7688a5fc06115ba6c3f5e8fed65cca2f2c
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-09-09 18:12:54 +0200

    Version 1.0.4

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ static: 'public' });
+const path = require('path');
+const middlewares = jsonServer.defaults({ static: path.join(__dirname, 'public') });
+
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
