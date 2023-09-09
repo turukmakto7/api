eac416970499b854c5ff585d25c1ac1186ba4377
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-09-09 17:50:35 +0200

    Update 2

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json --static ./landing');
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults(({
+    static: landing,
+}));
 const port = process.env.PORT || 3200;
 
 server.use(middlewares);
