5e47b2c6d9137a630f2c8ea50eb7060657261d6c
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-09-11 21:51:20 +0200

    Update

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const lowDb = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const bodyParser = require('body-parser')
+const { nanoid } = require('nanoid')
+
+const db = lowDb(new FileSync('db.json'))
+db.defaults({}).write()
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(db);
 const path = require('path');
 const middlewares = jsonServer.defaults({ static: path.join(__dirname, 'public') });
 
@@ -17,7 +30,11 @@ server.use(jsonServer.rewriter({
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
     if (req.method === 'POST') {
-        req.body.createdAt = Date.now()
+        const note = req.body
+        db.get('notes').push({
+            ...note, id: nanoid()
+        }).write()
+        res.json({ success: true })
     }
     // Continue to JSON Server router
     next()
