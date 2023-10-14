648ac1eb9dfa2099ceebba64722c17014c3c82a4
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-10-14 22:08:19 +0200

    Version 1.1.1

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 
 // Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join('db.json');
+const data = fs.readFileSync(filePath, 'utf-8');
 const db = JSON.parse(data);
-const router = jsonServer.router(db)
+const router = jsonServer.router(db);
 
-const middlewares = jsonServer.defaults({ static: path.join(__dirname, 'public') })
+const middlewares = jsonServer.defaults({ static: path.join(__dirname, 'public') });
 
-server.use(middlewares)
+server.use(middlewares);
 
 server.use(jsonServer.rewriter({
     '/quotes/:category': '/quotes?category=:category',
     '/billing/card/:type': '/billing?card_type=:type',
     '/tasks/category/:category': '/tasks?category=:category',
     '/tasks/:id': '/tasks?id=:id',
-}))
+}));
 
-server.use(router)
+server.use(router);
 server.listen(3000, () => {
     console.log('JSON Server is running')
-})
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
