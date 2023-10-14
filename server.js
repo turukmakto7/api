55bfd4700bdc66925b39ac5dac3871f59c39086b
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2023-10-14 22:31:47 +0200

    Version 1.1.2

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
 
 // Uncomment to allow write operations
-const fs = require('fs');
-const path = require('path');
-const filePath = path.join('db.json');
-const data = fs.readFileSync(filePath, 'utf-8');
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db);
+const router = jsonServer.router(db)
 
-const middlewares = jsonServer.defaults({ static: path.join(__dirname, 'public') });
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
-server.use(middlewares);
+const middlewares = jsonServer.defaults({ static: path.join(__dirname, 'public') })
 
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/quotes/:category': '/quotes?category=:category',
-    '/billing/card/:type': '/billing?card_type=:type',
-    '/tasks/category/:category': '/tasks?category=:category',
-    '/tasks/:id': '/tasks?id=:id',
-}));
-
-server.use(router);
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
-});
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
